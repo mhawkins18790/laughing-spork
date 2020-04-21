69fb188d4f03fe95cf1a28353f1e8f2acbcb93ff
Matt Hawkins <mhawkins18790@gmail.com>
2020-04-21 16:08:36 -0400

    april 21

--- Chapter Four.py
+++ Chapter Four.py
@@ -1,3 +1,23 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician)
+
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+
+for value in range(1, 5):
+    print(value)
+
+squares = []
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+
+squaresTwo = [value**2 for value in range(1, 11)]
+print(squaresTwo)
+
+# onemillion = []
+# for value in range(1, 1000001):
+#     onemillion.append(value)
