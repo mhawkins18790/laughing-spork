0f3d4277955fe595705d68e37322accb829b24df
mhawkins18790 <mhawkins18790@gmail.com>
2020-12-08 10:34:25 -0500

    December 8, 2020

---
+++ Django_Practice/learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # Home page
+    url(r'^$', views.index, name='index'),
+
+    # Show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+
+]
